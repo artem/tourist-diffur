025698a327e761b0d3522edde4ec988a4d2442dd
Artem Labazov <123321artyom@gmail.com>
2020-11-28 00:18:42 +0300

    Generate the data randomly for demo

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -6,40 +6,14 @@ import org.knowm.xchart.style.Styler;
 import org.knowm.xchart.style.Styler.LegendPosition;
 import org.knowm.xchart.style.markers.SeriesMarkers;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
 public class Demo {
-    private final static double[] xAges =
-            new double[]{
-                    60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81,
-                    82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100
-            };
-
-    private final static double[] yPercentile75th =
-            new double[]{
-                    800000, 878736, 945583, 1004209, 1083964, 1156332, 1248041, 1340801, 1440138, 1550005,
-                    1647728, 1705046, 1705032, 1710672, 1700847, 1683418, 1686522, 1674901, 1680456, 1679164,
-                    1668514, 1672860, 1673988, 1646597, 1641842, 1653758, 1636317, 1620725, 1589985, 1586451,
-                    1559507, 1544234, 1529700, 1507496, 1474907, 1422169, 1415079, 1346929, 1311689, 1256114,
-                    1221034
-            };
-
-    private final static double[] xAges1 =
-            new double[]{
-                    60, 61, 62, 63, 64, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81,
-                    82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97
-            };
-
-    private final static double[] yPercentile75th1 =
-            new double[]{
-                    800000, 1083964, 1156332, 1248041, 1340801, 1440138, 1550005,
-                    1647728, 1705046, 1705032, 1710672, 1700847, 1683418, 1686522, 1674901, 1680456, 1679164,
-                    1668514, 1672860, 1673988, 1646597, 1641842, 1653758, 1636317, 1620725, 1589985, 1586451,
-                    1559507, 1544234, 1422169, 1415079, 1346929, 1311689, 1256114,
-                    1221034
-            };
+    private final List<Integer> xAges = new ArrayList<>();
+    private final List<Integer> yPercentile75th = new ArrayList<>();
+    private final Random rand = new Random();
     private final XYChart xyChart;
+    private int counter;
 
     public Demo() {
         xyChart = getChart();
@@ -56,7 +30,6 @@ public class Demo {
 
         TimerTask chartUpdaterTask =
                 new TimerTask() {
-
                     @Override
                     public void run() {
                         updateData();
@@ -65,7 +38,7 @@ public class Demo {
                 };
 
         Timer timer = new Timer();
-        timer.scheduleAtFixedRate(chartUpdaterTask, 2000, 500);
+        timer.scheduleAtFixedRate(chartUpdaterTask, 0, 100);
     }
 
     public XYChart getChart() {
@@ -83,10 +56,12 @@ public class Demo {
         chart.getStyler().setLegendPosition(LegendPosition.InsideNW);
         chart.getStyler().setDefaultSeriesRenderStyle(XYSeriesRenderStyle.Line);
         chart.getStyler().setYAxisLabelAlignment(Styler.TextAlignment.Right);
-        chart.getStyler().setYAxisDecimalPattern("$ #,###.##");
+        //chart.getStyler().setYAxisDecimalPattern("$ #,###.##");
         chart.getStyler().setPlotMargin(0);
         chart.getStyler().setPlotContentSize(.95);
 
+        xAges.add(counter++);
+        yPercentile75th.add(3234);
         // Series
         chart.addSeries("75th Percentile", xAges, yPercentile75th).setMarker(SeriesMarkers.NONE);
 
@@ -94,6 +69,12 @@ public class Demo {
     }
 
     public void updateData() {
-        xyChart.updateXYSeries("75th Percentile", xAges1, yPercentile75th1, null);
+        xAges.add(counter++);
+        yPercentile75th.add(Math.abs(rand.nextInt()) % 42069);
+        if (xAges.size() > 20) {
+            xAges.remove(0);
+            yPercentile75th.remove(0);
+        }
+        xyChart.updateXYSeries("75th Percentile", xAges, yPercentile75th, null);
     }
 }
