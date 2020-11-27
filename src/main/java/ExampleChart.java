9f74235f42c48b76d355ea69f3f25fde5ed5ecde
Artem Labazov <123321artyom@gmail.com>
2020-11-27 22:53:53 +0300

    Add Demo

---
+++ src/main/java/ExampleChart.java
@@ -0,0 +1,8 @@
+import org.knowm.xchart.internal.chartpart.Chart;
+
+public interface ExampleChart<C extends Chart<?, ?>> {
+
+  C getChart();
+
+  String getExampleChartName();
+}
---
+++ src/main/java/RealtimeExampleChart.java
@@ -0,0 +1,4 @@
+public interface RealtimeExampleChart {
+
+  public void updateData();
+}
